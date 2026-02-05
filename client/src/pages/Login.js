ad59814f67e2c16ff637de66f682a4217e580125
CoderHarshaVardhan <2004.Harsha.Vardhan.2004@gmail.com>
2026-02-05 12:25:32 +0530

    a

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { motion } from 'framer-motion';
 import LoadingSpinner from '../components/LoadingSpinner';
